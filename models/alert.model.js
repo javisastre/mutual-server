280f698d2346f40a29341ab883f23b86076aa62a
Javi Sastre <javisastrecal@gmail.com>
2021-03-07 18:16:15 +0100

    alert model timestamp fixed

--- models/alert.model.js
+++ models/alert.model.js
@@ -1,16 +1,24 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const alertSchema = new Schema({
-  person: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  active: { type: Boolean, default: true },
-  location: { type: [], default: [41.39, 2.15] },
-  public: { type: Boolean, default: false },
-  category: { type: [] },
-  story: { type: String, default: "" },
-  hour: { type: String, default: "" },
-  date: { type: String, default: "" },
-});
+const alertSchema = new Schema(
+  {
+    person: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    active: { type: Boolean, default: true },
+    location: { type: [], default: [41.39, 2.15] },
+    public: { type: Boolean, default: false },
+    category: { type: [] },
+    story: { type: String, default: "" },
+    hour: { type: String, default: "" },
+    date: { type: String, default: "" },
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
 
 const Alert = mongoose.model("Alert", alertSchema);
 
