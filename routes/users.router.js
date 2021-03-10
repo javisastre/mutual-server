e4c6d6709fa33d86d1a540916f73c73fabd3013a
Javi Sastre <javisastrecal@gmail.com>
2021-03-10 16:35:48 +0100

    all users route

--- routes/users.router.js
+++ routes/users.router.js
@@ -7,12 +7,20 @@ const User = require("../models/user.model");
 
 const { isLoggedIn } = require("../helpers/middleware");
 
-router.get("/user", isLoggedIn, async (req, res, next) => {
+router.get("/user/:userId", isLoggedIn, async (req, res, next) => {
   try {
-    const { userId } = req.body;
+    const { userId } = req.params;
     const foundUser = await User.findById(userId);
-
     res.status(201).json(foundUser);
+  } catch (error) {
+    next(createError(error));
+  }
+});
+
+router.get("/all", isLoggedIn, async (req, res, next) => {
+  try {
+    const allUsers = await User.find();
+    res.status(201).json(allUsers);
   } catch (error) {
     next(createError(error));
   }
